fbe70208348b0615050cfae8a4a2cf5351915df8
Dipesh <123dipeshsingh@gmail.com>
2023-01-29 17:09:39 +0530

    footer section funtionality added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,15 +5,17 @@ import React from "react";
 const Footer = () => {
   return <div className="footer">
     <div className="icon">
-    <i className="fa-brands fa-github"></i>
+      <a href="https://github.com/Dipesh-007"> <i className="fa-brands fa-github iconhover" ></i></a>
+   
     </div>
    <div className="icon">
-    <i className="fa-brands fa-linkedin"></i>
+    <a href="https://www.linkedin.com/in/dipesh-singh-651b67247/"><i className="fa-brands fa-linkedin iconhover"></i></a>
+    
 
    </div>
 
    <div className="icon">
-   <i className="fa-solid fa-envelope i"></i>
+   <i className="fa-solid fa-envelope iconhover"></i>
    </div>
 
   </div>;
